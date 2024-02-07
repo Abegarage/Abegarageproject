7af0b8c7ebdbc66bee6d5f821751284b9182e7f4
beltobel <beletebogale2@gmail.com>
2024-02-07 16:45:49 +0300

    update to: dbConfig, employee controller, emp services, index,emp routes, env and app js

--- Backend/config/dbConfig.js
+++ Backend/config/dbConfig.js
@@ -1,14 +1,34 @@
-require("dotenv").config();
-const mysql = require("mysql2");
+// require("dotenv").config();
+// const mysql = require("mysql2");
 
-const dbConnection = mysql.createPool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
+// const dbConnection = mysql.createPool({
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+//   host: process.env.DB_HOST,
+//   connectionLimit: 10,
+// });
+
+// module.exports = dbConnection.promise();
+
+
+// Import the mysql2 module Promise Wrapper 
+const mysql = require('mysql2/promise');
+// Prepare connection parameters we use to connect to the database
+const dbConfig = {
   connectionLimit: 10,
-});
-
-module.exports = dbConnection.promise();
-
-
+  // socketPath: process.env.DB_SOCKET_PATH,
+  password: process.env.DB_PASS,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+}
+// Create the connection pool  
+const pool = mysql.createPool(dbConfig);
+// Prepare a function that will execute the SQL queries asynchronously
+async function query(sql, params) {
+  const [rows, fields] = await pool.execute(sql, params);
+  return rows;
+}
+// Export the query function for use in the application 
+module.exports = { query };
