0fb9e20c9cd17f64ebbbd9cc44d50c6f13ebe5bb
AronDaniel97 <132969208+AronDaniel97@users.noreply.github.com>
2024-02-08 13:21:26 -0500

    Update dbConfig.js

--- Backend/config/dbConfig.js
+++ Backend/config/dbConfig.js
@@ -1,18 +1,4 @@
-// require("dotenv").config();
-// const mysql = require("mysql2");
 
-// const dbConnection = mysql.createPool({
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: process.env.DB_NAME,
-//   host: process.env.DB_HOST,
-//   connectionLimit: 10,
-// });
-
-// module.exports = dbConnection.promise();
-
-
-// Import the mysql2 module Promise Wrapper 
 const mysql = require('mysql2/promise');
 // Prepare connection parameters we use to connect to the database
 const dbConfig = {
